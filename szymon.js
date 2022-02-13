4f97afb999c110d09a999e30d36147693bce03da
SzymonCw <98704699+SzymonCw@users.noreply.github.com>
2022-02-13 21:44:17 +0100

    Add files via upload

---
+++ szymon.js
@@ -0,0 +1,20 @@
+﻿
+let wartosd = document.querySelector('nogi');
+let rezultat = document.querySelector('wielkosc');
+
+function myFunction() {
+
+   var buttonValue = document.getElementById('nogi').value;
+   // var dodawanie = buttonValue * 5;
+   result.value = document.getElementById("one").innerText="Your Text Here"
+
+ //   document.getElementById('wielkosc') = dodawanie;
+       
+  //  alert(dodawanie);
+}
+
+function ik(){
+   var buttonValue = document.getElementById('nogi').value;
+    result = document.getElementById('one');
+    result.value =  Math.floor(parseInt(buttonValue)*0.63/2.54);  
+ }
